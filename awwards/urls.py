f838d1bfa9e45a0cf95c0645493d346d207a70ee
jerushaotieno <jerushaotienocoding@gmail.com>
2022-04-09 17:17:21 +0300

    introduce dynamic content and template inheritance to the app

--- awwards/urls.py
+++ awwards/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url('^today/$',views.projects_of_day,name='projectsToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_projects,name = 'pastProjects')
 ]
--- awwards/views.py
+++ awwards/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.http  import HttpResponse,Http404
+from django.shortcuts import render, redirect
+import datetime as dt
 
 # Create your views here.
 
+# Welcome greeting
 def welcome(request):
     return render(request, 'index.html')
+
+
+# View Function to present projects from today
+
+def projects_of_day(request):
+    date = dt.date.today()
+    return render(request, 'today-projects.html', {"date": date,})
+
+
+# View Function to present projects from past days
+
+def past_days_projects(request, past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(projects_of_day)
+
+    return render(request, 'past-projects.html', {"date": date})
