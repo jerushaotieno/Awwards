9414ec07b06d78d26cc49c65f629007e8221126b
jerushaotieno <jerushaotienocoding@gmail.com>
2022-04-11 15:28:02 +0300

    add a registration form for signing up

--- rateproject/urls.py
+++ rateproject/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('awwards.urls'))
+    url(r'',include('awwards.urls')),
+    # url(r'^accounts/', include('django-registration.backends.simple.urls')),
 ]
