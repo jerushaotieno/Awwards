f82d4a6cdc668bc6b644ccc62db72c67c4f639a8
jerushaotieno <jerushaotienocoding@gmail.com>
2022-04-11 08:43:07 +0300

    add code to upload and display uploaded images

--- awwards/urls.py
+++ awwards/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.projects_today,name='projectsToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_projects,name = 'pastProjects'),
     url(r'^search/', views.search_results, name='search_results')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
